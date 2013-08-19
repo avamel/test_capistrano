61bf40ca6b6e51d5e2bdf18f808abb849723ab1e
avamel <xnuamx@gmail.com>
2013-08-19 14:18:49 +0300

    fixing capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,37 +20,37 @@ set :rvm_type, :system
 
 server "37.139.29.126", :app, :web, :db, :primary => true
 
-# Generate an additional task to fire up the thin clusters
-#namespace :deploy do
-#  desc "Start the Thin processes"
-#  task :start do
-#    run  <<-CMD
-#      cd /var/www/test_capistrano/current; bundle exec thin start -C config/thin.yml
-#    CMD
-#  end
-#
-#  desc "Stop the Thin processes"
-#  task :stop do
-#    run <<-CMD
-#      cd /var/www/test_capistrano/current; bundle exec thin stop -C config/thin.yml
-#    CMD
-#  end
-#
-#  desc "Restart the Thin processes"
-#  task :restart do
-#    run <<-CMD
-#      cd /var/www/test_capistrano/current; bundle exec thin restart -C config/thin.yml
-#    CMD
-#  end
-#
-#end
+Generate an additional task to fire up the thin clusters
+namespace :deploy do
+  desc "Start the Thin processes"
+  task :start do
+    run  <<-CMD
+      cd /var/www/test_capistrano/current; bundle exec thin start -C config/thin.yml
+    CMD
+  end
 
-#namespace :rvm do
-#  task :trust_rvmrc do
-#    run "rvm rvmrc trust #{release_path}"
-#  end
-#end
+  desc "Stop the Thin processes"
+  task :stop do
+    run <<-CMD
+      cd /var/www/test_capistrano/current; bundle exec thin stop -C config/thin.yml
+    CMD
+  end
+
+  desc "Restart the Thin processes"
+  task :restart do
+    run <<-CMD
+      cd /var/www/test_capistrano/current; bundle exec thin restart -C config/thin.yml
+    CMD
+  end
+
+end
+
+namespace :rvm do
+  task :trust_rvmrc do
+    run "rvm rvmrc trust #{release_path}"
+  end
+end
 
 # Define all the tasks that need to be running manually after Capistrano is finished.
 after "deploy", "deploy:migrate"
-#after "deploy:migrate", "rvm:trust_rvmrc"
+after "deploy:migrate", "rvm:trust_rvmrc"
